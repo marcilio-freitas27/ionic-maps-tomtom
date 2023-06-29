2d1b0eb0b1da5d7059336f0f1045dbc57dc5247e
Marcilio Freitas <marcilio@beltsistemas.com.br>
2023-06-29 14:03:50 -0300

    Initial commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import tt from '@tomtom-international/web-sdk-maps';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  standalone: true,
+  imports: [IonicModule, ExploreContainerComponent],
+})
+export class Tab1Page {
+  map!: any;
+  constructor() {
+    this.map = tt.map({
+      key: 'qWs3zqjNxKQGEexONPwFAxRE5knGm6K7',
+      container: 'map',
+      center: new tt.LngLat(-35, -5),
+      zoom: 12,
+    });
+  }
+}
